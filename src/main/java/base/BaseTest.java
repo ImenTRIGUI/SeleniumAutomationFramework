5cf8a7019a7c87c44ddfc8f40db3a778584bd08f
itrigui <triguiimen99@gmail.com>
2025-03-03 15:09:15 +0100

    second commit

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import utils.Log;
+
 public class BaseTest {
 	
 	protected WebDriver driver;
@@ -12,17 +14,20 @@ public class BaseTest {
 	@BeforeMethod
 	public void setUp() {
 	
+		Log.info("Starting WebDriver");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
+		Log.info("Navigation to URL....");
 		driver.get("https://admin-demo.nopcommerce.com/login");		
 	}
-/*
+
 	@AfterMethod
 	public void tearDown(){
 		if (driver != null) {
+			Log.info("Closing Browser.");
 			driver.quit();
 		}
-	} */
+	} 
 
 
 }
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -5,18 +5,25 @@ import org.testng.annotations.Test;
 
 import base.BaseTest;
 import pages.LoginPage;
+import utils.Log;
 
 public class LoginTest extends BaseTest {
 	
 	@Test
 	public void testValidLogin() {
+		
+		Log.info("Starting login test...");
 		LoginPage loginPage = new LoginPage(driver);
 		
+		
+		Log.info("Adding credentials");
 		loginPage.enterUsername("admin@yourstore.com");
 		loginPage.enterPassword("admin");
 		loginPage.clickLogin();
-		System.out.println("The title of the page is: "+ driver.getTitle());
 		
+		
+		System.out.println("The title of the page is: "+ driver.getTitle());
+		Log.info("Verifying page title");
 		Assert.assertEquals(driver.getTitle(), "Un instant…");
 	}
 
